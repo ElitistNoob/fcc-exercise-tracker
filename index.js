6259d0e440d462d205591f243383ebc466364091
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-26 18:28:36 +0700

    added post to api/users

--- index.js
+++ index.js
@@ -15,8 +15,21 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const userSchema = mongoose.Schema({
+  username: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(BASE_URL);
+});
+
+app.post('/api/users', async (req, res) => {
+  await User.create({ username: req.body.username });
+  const userQuery = await User.findOne({ username: req.body.username });
+  const { username, _id } = userQuery;
+  res.send({ username, _id });
 });
 
 app.listen(process.env.PORT || 3000);
